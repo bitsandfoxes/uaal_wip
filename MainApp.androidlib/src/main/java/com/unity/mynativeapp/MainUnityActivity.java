574e7e10aaf496e8fd4410d20a8955b7d77941b4
bitsandfoxes <reg@bitfox.at>
2024-11-11 11:46:16 +0100

    cleanup

--- MainApp.androidlib/src/main/java/com/unity/mynativeapp/MainUnityActivity.java
+++ MainApp.androidlib/src/main/java/com/unity/mynativeapp/MainUnityActivity.java
@@ -2,33 +2,14 @@ package com.unity.mynativeapp;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 
 import com.unity3d.player.UnityPlayerActivity;
 
 public class MainUnityActivity extends UnityPlayerActivity {
 
-    private String appendCommandLineArgument(String cmdLine, String arg) {
-        if (arg == null || arg.isEmpty())
-            return cmdLine;
-        else if (cmdLine == null || cmdLine.isEmpty())
-            return arg;
-        else
-            return cmdLine + " " + arg;
-    }
-
-    protected String updateUnityCommandLineArguments(String cmdLine)
-    {
-//        return appendCommandLineArgument(cmdLine, "-androidChainedSignalHandlerBehavior=disabled");
-        return cmdLine;
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        String cmdLine = updateUnityCommandLineArguments(getIntent().getStringExtra("unity"));
-        getIntent().putExtra("unity", cmdLine);
 
         // Setup activity layout
         SharedClass.addControlsToUnityFrame(this);
